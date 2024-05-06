9e22b6546c321aef94c7cda293c569cc127ebf25
JaeDoo <fosong98@gmail.com>
2024-05-06 23:47:18 +0900

    :tada:init: 관통 프로젝트 시작.

---
+++ src/main/java/com/ssafy/trippals/TrippalsApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.trippals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrippalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/trippals/TrippalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.trippals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
