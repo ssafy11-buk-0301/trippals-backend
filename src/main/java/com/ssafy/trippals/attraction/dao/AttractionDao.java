53cffc217c09f07da46d7cc2b2d8f1cbda0856a2
JaeDoo <fosong98@gmail.com>
2024-05-15 21:26:33 +0900

    :sparkles: 여행지 조회 관련 Dao 쿼리 작성

---
+++ src/main/java/com/ssafy/trippals/attraction/dao/AttractionDao.java
@@ -0,0 +1,21 @@
+package com.ssafy.trippals.attraction.dao;
+
+import com.ssafy.trippals.attraction.dto.*;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AttractionDao {
+    List<AttractionData> findByRouteSeq(int routeSeq);
+
+    List<AttractionData> findByKeyword(AttractionKeywordSelect attractionKeywordSelect);
+    List<AttractionData> findBySidoAndKeyword(AttractionSidoAndKeywordSelect attractionKeywordSelect);
+    List<AttractionData> findByGugunAndKeyword(AttractionGugunAndKeywordSelect attractionKeywordSelect);
+
+    int countByKeyword(AttractionKeywordSelect attractionKeywordSelect);
+    int countBySidoAndKeyword(AttractionSidoAndKeywordSelect attractionKeywordSelect);
+    int countByGugunAndKeyword(AttractionGugunAndKeywordSelect attractionKeywordSelect);
+
+    List<AttractionData> findNearbyAttractionsByContentType(NearByAttractionContentTypeSelect attractionSelect);
+}
