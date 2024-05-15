6330e0b2fd6ec9ef38789c65696adf63c9050d1d
pyh007264 <pyh007264@gmail.com>
2024-05-15 22:47:09 +0900

    :white_check_mark:test:게시글 테스트 코드 생성

--- src/main/java/com/ssafy/trippals/board/dto/BoardParams.java
+++ src/main/java/com/ssafy/trippals/board/dto/BoardParams.java
@@ -1,8 +1,10 @@
 package com.ssafy.trippals.board.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BoardParams {
     private Integer limit;
     private Integer offset;
