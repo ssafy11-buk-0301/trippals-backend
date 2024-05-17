4da961bce3074ddd9c37ca89700c03aa86d57d2a
YeongHunPark <pyh007264@gmail.com>
2024-05-17 12:02:46 +0900

    :sparkles: feat: 기본 CRUD (without 파일첨부,경로첨부)

--- src/main/java/com/ssafy/trippals/board/dao/BoardDao.java
+++ src/main/java/com/ssafy/trippals/board/dao/BoardDao.java
@@ -1,22 +1,18 @@
 package com.ssafy.trippals.board.dao;
 
-import com.ssafy.trippals.board.dto.BoardData;
-import com.ssafy.trippals.board.dto.BoardInsert;
+import com.ssafy.trippals.board.dto.BoardDto;
 import com.ssafy.trippals.board.dto.BoardParams;
-import com.ssafy.trippals.board.dto.BoardUpdate;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface BoardDao {
-    List<BoardData> findBoardData(BoardParams boardParams);
-    BoardData findBoardBySeq(Integer boardSeq);
-    Integer insertBoard(BoardInsert boardInsert);
-    Integer updateBoard(BoardUpdate boardUpdate);
+    List<BoardDto> findBoardData(BoardParams boardParams);
+    BoardDto findBoardBySeq(Integer boardSeq);
+    Integer insertBoard(BoardDto boardDto);
+    Integer updateBoard(BoardDto boardDto);
     Integer deleteBoardBySeq(Integer boardSeq,Integer userSeq);
-
     Integer boardListCount();
-
     Integer boardListSearchWordCount(String searchWord);
 }
--- src/main/java/com/ssafy/trippals/board/dto/BoardDto.java
+++ src/main/java/com/ssafy/trippals/board/dto/BoardDto.java
@@ -2,15 +2,19 @@ package com.ssafy.trippals.board.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-public class BoardInsert {
+public class BoardDto {
+    private Integer seq;
     private Integer userSeq;
     private String title;
     private String content;
-    private boolean isDraft;//임시 여부 controller에서 결정
+    private LocalDateTime regDt;
+    private boolean isDraft;
     private Integer routeSeq;
-
-
 }
