b5ef8f75fe506a7cacb5299e28123f2c6bd3749d
YeongHunPark <pyh007264@gmail.com>
2024-05-17 10:48:46 +0900

    :sparkles: feat: CRUD, Count 추가

--- src/main/java/com/ssafy/trippals/board/dao/BoardDao.java
+++ src/main/java/com/ssafy/trippals/board/dao/BoardDao.java
@@ -3,6 +3,7 @@ package com.ssafy.trippals.board.dao;
 import com.ssafy.trippals.board.dto.BoardData;
 import com.ssafy.trippals.board.dto.BoardInsert;
 import com.ssafy.trippals.board.dto.BoardParams;
+import com.ssafy.trippals.board.dto.BoardUpdate;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,8 +11,12 @@ import java.util.List;
 @Mapper
 public interface BoardDao {
     List<BoardData> findBoardData(BoardParams boardParams);
-    BoardData findBoardBySeq(Integer board_seq);
-    int insertBoard(BoardInsert boardInsert);
-    int updateboard(BoardInsert boardUpdate);
-    int deleteBoard(Integer board_seq);
+    BoardData findBoardBySeq(Integer boardSeq);
+    Integer insertBoard(BoardInsert boardInsert);
+    Integer updateBoard(BoardUpdate boardUpdate);
+    Integer deleteBoardBySeq(Integer boardSeq,Integer userSeq);
+
+    Integer boardListCount();
+
+    Integer boardListSearchWordCount(String searchWord);
 }
