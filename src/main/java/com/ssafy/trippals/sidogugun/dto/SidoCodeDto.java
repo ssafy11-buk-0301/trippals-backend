752601c871047452dbf104c10ba62e57248d3380
jaedoo <fosong98@gmail.com>
2024-05-21 15:54:51 +0900

    :sparkles: feat: 시도, 구군 코드 조회 기능 추가

---
+++ src/main/java/com/ssafy/trippals/sidogugun/dto/SidoCodeDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.trippals.sidogugun.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SidoCodeDto {
+    private String sidoCode;
+    private String sidoName;
+}
