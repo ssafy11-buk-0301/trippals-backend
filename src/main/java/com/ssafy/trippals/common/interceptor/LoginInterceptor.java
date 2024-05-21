d69338342cb859dd177ae93cf0eebca59905df69
pyh007264 <pyh007264@gmail.com>
2024-05-22 05:42:06 +0900

    :boom: change: 게시글 수정 버그 해결

--- src/main/java/com/ssafy/trippals/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/ssafy/trippals/common/interceptor/LoginInterceptor.java
@@ -1,8 +1,12 @@
 package com.ssafy.trippals.common.interceptor;
 
+import com.ssafy.trippals.SessionConst;
+import com.ssafy.trippals.common.exception.UserAuthException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,16 +15,16 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
-//            return true;
-//        }
-//        log.debug("ipaddr: {}", request.getRequestURI());
-//        HttpSession session = request.getSession(false);
-//        if (session == null) throw new UserAuthException();
-//
-//        Object user = session.getAttribute(SessionConst.USER);
-//
-//        if (user == null) throw new UserAuthException();
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
+        log.debug("ipaddr: {}", request.getRequestURI());
+        HttpSession session = request.getSession(false);
+        if (session == null) throw new UserAuthException();
+
+        Object user = session.getAttribute(SessionConst.USER);
+
+        if (user == null) throw new UserAuthException();
 
         return true;
     }
