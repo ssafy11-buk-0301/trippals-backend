c8ac66aaa2a4c1bc488dc33865f30894c2b4c3b9
YeongHunPark <pyh007264@gmail.com>
2024-05-23 17:53:17 +0900

    :boom: change: baseUrl 수정, 발표용

--- src/main/java/com/ssafy/trippals/common/config/WebConfig.java
+++ src/main/java/com/ssafy/trippals/common/config/WebConfig.java
@@ -34,7 +34,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174")
+                .allowedOrigins("http://192.168.203.126:81", "http://192.168.203.126:5174")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "TRACE")
                 .allowCredentials(true);
     }
--- src/main/java/com/ssafy/trippals/common/config/WebSocketConfig.java
+++ src/main/java/com/ssafy/trippals/common/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("http://192.168.203.126:81")
                 .withSockJS();
     }
 }
